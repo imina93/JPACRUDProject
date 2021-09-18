c35d611b51b97fdcafb4fe1d6ba84323eaf8e4f1
Isaac Mina <isaacmina93@gmail.com>
2021-09-17 17:04:35 -0700

    Create Boot MVC project

---
+++ MonsterGallery/src/main/java/com/skilldistillery/jpacrud/MonsterGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterGalleryApplication.class, args);
+	}
+
+}
